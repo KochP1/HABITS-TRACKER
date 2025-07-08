dd9d9e4266e2c2ed812b11704afe59d527696506
kochp1 <juanandreskochp@gmail.com>
2025-07-07 20:09:01 -0400

    Creation of habit model

---
+++ server/src/models/habit.model.ts
@@ -0,0 +1,73 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/db";
+
+
+interface HabitAttributes {
+    id?: number
+    name: string
+    color: string
+    question: string
+    frequency: string
+    unit?: string
+    target?: number
+    target_type?:  string
+    type: string
+}
+
+class Habit extends Model<HabitAttributes> implements HabitAttributes {
+    public id!: number;
+    public name!: string
+    public color!: string
+    public question!: string
+    public frequency!: string
+    public unit!: string
+    public target!: number
+    public target_type!:  string
+    public type!: string
+}
+
+Habit.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    color: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    question: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    frequency: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    unit: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    target: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    target_type: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    type: {
+        type: DataTypes.ENUM,
+        allowNull: false
+    }
+
+},{
+        sequelize,
+        modelName: 'Habit',
+        tableName: 'habits',
+        timestamps: false,
+    })
