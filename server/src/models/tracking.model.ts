698c72555bf74cbe89280f7c44503a5683c5c1b8
kochp1 <juanandreskochp@gmail.com>
2025-07-08 12:42:24 -0400

    Creating tracking sequelize model

---
+++ server/src/models/tracking.model.ts
@@ -0,0 +1,43 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/db";
+
+interface TrackerAttributes {
+    id?: number
+    habits_id: number
+    date: string
+    done: string
+}
+
+export class Tracker extends Model<TrackerAttributes> implements TrackerAttributes {
+    public id!: number;
+    public habits_id!: number;
+    public date!: string;
+    public done!: string
+}
+
+Tracker.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    habits_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    date: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    done: {
+        type: DataTypes.ENUM('yes', 'no'),
+        allowNull: false,
+        defaultValue: 'no'
+    }
+}, {
+        sequelize,
+        modelName: 'Tracking',
+        tableName: 'tracking',
+        timestamps: false,
+    }
+);
